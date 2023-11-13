e68b8e902ad108a4e863cd7d11ecb8368cc44767
salinpaul valooran <salinpv@hotmail.com>
2023-11-13 20:38:50 +0530

    function for updating time

--- version_01/index.js
+++ version_01/index.js
@@ -1,2 +1,7 @@
 let hours = document.getElementById("hours");
 let minutes = document.getElementById("minutes");
+let seconds = document.getElementById("seconds");
+
+function updateTime() {
+  let date = new Date();
+}
