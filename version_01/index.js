aebcb42204a0063b6cad72d9c12f2e04676ee727
salinpaul valooran <salinpv@hotmail.com>
2023-11-13 20:48:03 +0530

    adding 0 before hours if hours is less than 10

--- version_01/index.js
+++ version_01/index.js
@@ -6,7 +6,11 @@ function updateTime() {
   let date = new Date();
 
   hours.innerHTML =
-    date.getHours() > 12 ? date.getHours() - 12 : date.getHours();
+    date.getHours() > 12
+      ? date.getHours() > 10
+        ? `0${date.getHours() - 12}`
+        : date.getHours() - 12
+      : date.getHours();
   minutes.innerHTML = date.getMinutes();
   seconds.innerHTML = date.getSeconds();
 }
